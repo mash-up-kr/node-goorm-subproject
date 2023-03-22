ccc6010e82b6798bb57446d7bea68b4bcc7673e3
Anjonghun <58875626+JonghunAn@users.noreply.github.com>
2023-03-23 06:26:16 +0900

    feat: cicd shell script

--- cicd-script.sh
+++ cicd-script.sh
@@ -1,2 +1,17 @@
-// git pull origin develop
-// npm run start & back ground 실행 처리
+git fetch
+
+local=$(git rev-parse HEAD)
+echo $local
+
+target=$(git rev-parse origin/main)
+echo $target
+
+if [ $local != $target ]
+then
+        git stash
+        git pull origin master
+        echo 'remote changes pulled from master branch'
+        npm install
+        npm run build
+        npm run start
+fi
